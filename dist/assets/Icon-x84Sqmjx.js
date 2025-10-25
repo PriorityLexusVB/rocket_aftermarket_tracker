9467a96526f92bbb2f7713f6d4544ebeb7a4bd4c
Rob Brasco <rob.brasco@priorityautomotive.com>
2025-10-25 16:16:05 -0400

    feat(logging): implement application-wide logging utility and custom hook for enhanced user context logging
    - Added `useLogger` utility for comprehensive logging across the application.
    - Introduced `AppLogger` class to handle various log levels and actions.
    - Implemented logging for user actions, sales, services, and system events.
    - Created `useTenant` hook to retrieve organization ID from user profiles with error handling.
    - Developed `vendorService` for managing vendor data with search and retrieval functionalities.
    - Added source maps for better debugging and tracing of changes.

---
+++ dist/assets/Icon-x84Sqmjx.js
@@ -0,0 +1,2 @@
+import{j as t,aa as c,ab as n}from"./index-DgFDa27-.js";import"./react-Czq5O75u.js";const m=({name:a="Circle",size:e=16,className:i="",...o})=>{var r;const s=((r=c)==null?void 0:r[a])||n;return t.jsx(s,{size:e,className:i,"aria-hidden":!0,...o})};export{m as I};
+//# sourceMappingURL=Icon-x84Sqmjx.js.map
